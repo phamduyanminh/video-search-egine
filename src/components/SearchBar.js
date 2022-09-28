df91b25b34915153b0ce9a41eb604935a8223bc7
Minh Pham <phamduya@sheridancollege.ca>
2022-09-27 23:34:12 -0400

    Refactored search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import {useState} from 'react';
 
-class SearchBar extends React.Component{
-    state = {term: ''};
+const SearchBar = ({onFormSubmit}) => {
+    const [term, setTerm] = useState('');
 
-    onInputChange = (event) => {
-        this.setState({
-            term: event.target.value
-        });
+    const onInputChange = (event) => {
+        setTerm(event.target.value);
     }
 
-    onFormSubmit = (event) => {
+    const onSubmit = (event) => {
         event.preventDefault();
-        this.props.onFormSubmit(this.state.term);
+        onFormSubmit(term);
     }
 
-    render() {
-        return(
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input 
-                        placeholder='Please enter your search here'
-                        type="text" 
-                        value={this.state.term}
-                        onChange={this.onInputChange}
-                        >
-    
-                        </input>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
+
+    return(
+        <div className="search-bar ui segment">
+            <form onSubmit={onSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input 
+                    placeholder='Please enter your search here'
+                    type="text" 
+                    value={term}
+                    onChange={onInputChange}
+                    >
+
+                    </input>
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default SearchBar;
 
